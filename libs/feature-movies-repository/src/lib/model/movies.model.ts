01aa3d0b92cad7b339fab2eeae888b82b6e9e768
okonpiotr <okon.piotr@gmail.com>
2023-05-30 21:17:01 +0200

    basic model for movies

---
+++ libs/feature-movies-repository/src/lib/model/movies.model.ts
@@ -0,0 +1,26 @@
+export enum Country {
+  Ireland=  'IRELAND',
+  UK = 'UNITED_KINGDOM',
+}
+
+export interface UserMovies {
+  name: string,
+  username: string,
+  Country: Country,
+  postCode: string,
+  favouriteMovie: string,
+}
+
+export interface IMDBMovie {
+  "Title": string,
+  "Year": string,
+  "imdbID": string,
+  "Type": string,
+  "Poster": string
+}
+
+export interface IMDBResponse {
+  Search: IMDBMovie[],
+  totalResults: string;
+  Response: true;
+}
