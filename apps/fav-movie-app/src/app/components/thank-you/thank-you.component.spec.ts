7a6d46a940bb0d2cfd52be276f1951bff6ee8b4f
okonpiotr <okon.piotr@gmail.com>
2023-06-02 12:07:48 +0200

    initial tests

--- apps/fav-movie-app/src/app/components/thank-you/thank-you.component.spec.ts
+++ apps/fav-movie-app/src/app/components/thank-you/thank-you.component.spec.ts
@@ -1,9 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ThankYouComponent } from './thank-you.component';
+import { of } from "rxjs";
+import { ThankYouComponent } from "./thank-you.component";
+import { MovieFormModel } from "../model/movie-form.model";
 
-describe('ThankYouComponent', () => {
+fdescribe('MovieFormComponent', () => {
   let component: ThankYouComponent;
   let fixture: ComponentFixture<ThankYouComponent>;
+  let page: Page<ThankYouComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,10 +15,44 @@ describe('ThankYouComponent', () => {
 
     fixture = TestBed.createComponent(ThankYouComponent);
     component = fixture.componentInstance;
+    page = new Page(fixture);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('checks displaying of items ', ()=> {
+
+    it('should display name',  () => {
+      const data = {name: 'testName'} as MovieFormModel;
+      // Given
+      component.formData$ = of(data)
+
+      console.log('page.nameContainer', page.nameContainer)
+      // Then
+      expect(page.nameContainer).toBeTruthy();
+    });
+
+  })
+
 });
+
+class Page<T> {
+  private domRoot: HTMLElement;
+
+  constructor(private fixture: ComponentFixture<T>) {
+    this.domRoot = fixture.nativeElement;
+  }
+
+  get nameContainer(): HTMLDivElement {
+    return this.domRoot.querySelector(`h1`) as HTMLElement
+  }
+
+  get nameContainer(): HTMLDivElement {
+    return this.domRoot.querySelector(`[data-test-name]`) as HTMLDivElement
+  }
+
+
+}
